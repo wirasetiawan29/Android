39f04de78e574fa82b283feb5bf517e20a281e43
wira <wirasetiawan29@jojonomic.com>
2016-01-11 08:19:09 +0700

    FlipViewPager Update Utils
    Android Sample Apps

--- FlipViewPager/app/src/main/java/com/logbook/wira/flipviewpager/Utils.java
+++ FlipViewPager/app/src/main/java/com/logbook/wira/flipviewpager/Utils.java
@@ -13,12 +13,12 @@ public class Utils {
     public static final List<Makanan> makanan = new ArrayList<>();
 
     static {
-        makanan.add(new Makanan(R.drawable.soto, "SOTO", R.color.sienna, "Sport", "Literature", "Music", "Art", "Technology"));
-        makanan.add(new Makanan(R.drawable.soto, "NASI GORENG", R.color.saffron, "Travelling", "Flights", "Books", "Painting", "Design"));
-        makanan.add(new Makanan(R.drawable.soto, "RENDANG", R.color.green, "Sales", "Pets", "Skiing", "Hairstyles", "Сoffee"));
-        makanan.add(new Makanan(R.drawable.soto, "SATE", R.color.pink, "Android", "Development", "Design", "Wearables", "Pets"));
-        makanan.add(new Makanan(R.drawable.soto, "DENDENG", R.color.orange, "Design", "Fitness", "Healthcare", "UI/UX", "Chatting"));
-        makanan.add(new Makanan(R.drawable.soto, "SUP", R.color.saffron, "Development", "Android", "Healthcare", "Sport", "Rock Music"));
+        makanan.add(new Makanan(R.drawable.soto, "SOTO", R.color.sienna, "sup ayam", "soun", "telur ayam", "kol", "kentang"));
+        makanan.add(new Makanan(R.drawable.nasi_goreng, "NASI GORENG", R.color.saffron, "nasi", "telur", "bawang merah", "cabai rawit", "garam dan penyedap rasa"));
+        makanan.add(new Makanan(R.drawable.rendang, "RENDANG", R.color.green, "kelapa parut", "daging sapi", "air untuk santan", "cabai merah", "lengkuas"));
+        makanan.add(new Makanan(R.drawable.sate, "SATE", R.color.pink, "daging sapi", "asam", "gula merah", "bawang putih", "garam"));
+        makanan.add(new Makanan(R.drawable.dendeng, "DENDENG", R.color.orange, "daging sapi", "jahe", "bawang putih", "cabe merah", "bawang merah"));
+        makanan.add(new Makanan(R.drawable.sup, "SUP", R.color.saffron, "macaroni", "bawang bombay", "Tomat segar", "daging ayam", "Wortel"));
         makanan.add(new Makanan(R.drawable.soto, "MIE GORENG", R.color.green, "Cinema", "Music", "Tatoo", "Animals", "Management"));
         makanan.add(new Makanan(R.drawable.soto, "AYAM SINGGANG", R.color.purple, "Android", "IOS", "Application", "Development", "Company"));
     }
