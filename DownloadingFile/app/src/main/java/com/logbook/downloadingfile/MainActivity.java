058adeca367a6ac1233c3535aadb56ab7240dd44
wira <wirasetiawan29@jojonomic.com>
2016-01-18 14:48:21 +0700

    DownloadFile v0.1

--- DownloadingFile/app/src/main/java/com/logbook/downloadingfile/MainActivity.java
+++ DownloadingFile/app/src/main/java/com/logbook/downloadingfile/MainActivity.java
@@ -1,15 +1,39 @@
 package com.logbook.downloadingfile;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnShowProgress;
+
+    private ProgressDialog pDialog;
+    ImageView my_image;
+    private static final int progress_bar_type = 0;
+
+    private static String file_url = "https://wirasetiawan29.files.wordpress.com/2016/01/tentang-material-design1.png";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +42,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        my_image = (ImageView) findViewById(R.id.my_image);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                new DownloadFileFormURL().execute(file_url);
             }
         });
     }
@@ -49,4 +76,69 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected Dialog onCreateDialog(int id){
+        switch (id){
+            case progress_bar_type:
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Downloading file. Please Wait");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
+    }
+
+    class DownloadFileFormURL extends AsyncTask<String, String, String>
+
+    {
+        protected  void onPreExecute(){
+        super.onPreExecute();
+        showDialog(progress_bar_type);
+    }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+        int count;
+        try {
+            URL url = new URL(f_url[0]);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+
+            int lenghOfFile = connection.getContentLength();
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+            OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
+            byte data[] = new byte[1024];
+            long total = 0;
+
+            while ((count = input.read(data)) != -1){
+                total += count;
+                publishProgress(""+(int)((total*100)/lenghOfFile));
+                output.write(data, 0, count);
+            }
+            output.flush();
+            output.close();
+            input.close();
+        } catch (Exception e){
+            Log.e("Error : ", e.getMessage());
+        }
+        return null;
+    }
+
+    protected void onProgressUpdate(String... progress) {
+        pDialog.setProgress(Integer.parseInt(progress[0]));
+    }
+
+    protected void onPostExecute(String file_url){
+        dismissDialog(progress_bar_type);
+
+        String imagePath = Environment.getExternalStorageDirectory().toString() + "/downloadedfile.jpg";
+        my_image.setImageDrawable(Drawable.createFromPath(imagePath));
+    }
+}
 }
